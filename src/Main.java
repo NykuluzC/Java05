6b8904aeecf07afa4dbd3d3a9e7e9d83a13d4535
Nykuluz <carlmillanes3@gmail.com>
2023-06-26 04:51:14 +0800

    if,elseif and else statement with expression count

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
 public class Main {
-
+    public static void main(String[] args) {
+        int value = 1; //expression count: 1
+        if(value == 1) { //expression count: 1
+            System.out.println("Value was 1"); //expression count: 1
+        } else if(value == 2) { //expression count: 1
+            System.out.println("Value was 2"); //expression count: 1
+        } else {
+            System.out.println("Was not 1 or 2"); //expression count: 1
+        }
+    }
 }
-
