e2e35ceb125b31f21ae7c92eece17448537b19b9
Nykuluz <carlmillanes3@gmail.com>
2023-06-26 05:08:20 +0800

    Switch statements

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        int value = 1; //expression count: 1
-        if(value == 1) { //expression count: 1
-            System.out.println("Value was 1"); //expression count: 1
-        } else if(value == 2) { //expression count: 1
-            System.out.println("Value was 2"); //expression count: 1
-        } else {
-            System.out.println("Was not 1 or 2"); //expression count: 1
+        int value = 2; //expression count: 1
+
+        switch(value){
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3: case 4: case 5:
+                System.out.println("Was a 3, a 4, or a 5");
+                System.out.println("Actual value is : " + value);
+                break;
+            default:
+                System.out.println("Value was not 1, 2, 3, 4 or 5");
+                break;
         }
     }
 }
