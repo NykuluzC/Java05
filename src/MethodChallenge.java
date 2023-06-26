6194defbe5abb49427117cb0cd0638484d8ab228
Nykuluz <carlmillanes3@gmail.com>
2023-06-26 16:03:43 +0800

    Checking out Features

--- src/MethodChallenge.java
+++ src/MethodChallenge.java
@@ -5,16 +5,21 @@ public class MethodChallenge {
         //name: Irven, score 500
         //name: Fuentes, score 100
         //name: Millanes, score 25
+        String name = "Carl";
         int position  = calculateHighScorePosition(1000);
-        displayHighScorePosition("Carl",position);
+        displayHighScorePosition(name,position);
+        name = "Nykuluz";
         position = calculateHighScorePosition(1500);
-        displayHighScorePosition("Nykuluz",position);
+        displayHighScorePosition(name,position);
+        name = "Irven";
         position = calculateHighScorePosition(500);
-        displayHighScorePosition("Irven", position);
+        displayHighScorePosition(name, position);
+        name = "Fuentes";
         position = calculateHighScorePosition(100);
-        displayHighScorePosition("Fuentes", position);
+        displayHighScorePosition(name, position);
+        name = "Millanes";
         position = calculateHighScorePosition(25);
-        displayHighScorePosition("Millanes",position);
+        displayHighScorePosition(name,position);
     }
     public static void displayHighScorePosition(String name, int position){
         System.out.println(name + " managed to get into position " + position + " on the high score list.");
@@ -22,11 +27,15 @@ public class MethodChallenge {
     public static int calculateHighScorePosition(int score){
         if(score >= 1000){
             return 1;
-        } else if(score >= 500){
+        }
+        else if(score >= 500){
             return 2;
-        } else if(score >= 100){
+        }
+        else if(score >= 100){
             return 3;
         }
         return 4;
     }
+
+    //Just trying out local history in IntelliJ
 }
