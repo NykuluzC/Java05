0807b2819a7a283903e52e9974f12f4a02fdc5a5
Nykuluz <carlmillanes3@gmail.com>
2023-06-26 09:45:27 +0800

    Integer return method

--- src/Methods.java
+++ src/Methods.java
@@ -1,15 +1,19 @@
 public class Methods {
     public static void main(String[] args) {
-        calculateScore(true, 800,5,100);
-        calculateScore(true,10000, 8, 200);
+        //1st index
+        int highScore = calculateScore(true, 800,5,100);
+        System.out.println("The first high score is: " + highScore);
+        //2nd index
+        highScore = calculateScore(true,10000, 8, 200);
+        System.out.println("The next high score is: " + highScore);
     }
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
         int finalScore = score;
-
         if(gameOver){
             finalScore += (levelCompleted * bonus) + 1000;
-            System.out.println("Your final score was " + finalScore);
         }
+
+        return finalScore;
     }
 }
 
