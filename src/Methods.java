7672f4cd4e8d8aaa0854a6c687f650947e2ea623
Nykuluz <carlmillanes3@gmail.com>
2023-06-26 09:35:00 +0800

    Simplifying Method

--- src/Methods.java
+++ src/Methods.java
@@ -1,22 +1,11 @@
 public class Methods {
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
-
-        int finalScore = score;
-        calculateScore(gameOver,finalScore,levelCompleted,bonus);
-
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-
-        finalScore = score;
-
-        calculateScore(gameOver, finalScore, levelCompleted, bonus);
+        calculateScore(true, 800,5,100);
+        calculateScore(true,10000, 8, 200);
     }
-    public static void calculateScore(boolean gameOver, int finalScore, int levelCompleted, int bonus){
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+        int finalScore = score;
+
         if(gameOver){
             finalScore += (levelCompleted * bonus) + 1000;
             System.out.println("Your final score was " + finalScore);
